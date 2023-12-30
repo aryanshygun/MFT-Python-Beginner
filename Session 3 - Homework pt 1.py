fb0e11c706f8919cab167c756484eb950e9c4d8b
aryanshygun <70066717+aryanshygun@users.noreply.github.com>
2023-12-30 17:56:45 +0330

    Session 3 - Homework pt 1

---
+++ Session 3 - Homework pt 1.py
@@ -0,0 +1,17 @@
+###### Practice num1
+
+
+### x = 5
+### y = 4
+
+x = int(input(" Value for the length of the Rectangle:\n"))
+y = int(input(" Value for the width of the Rectangle:\n"))
+ 
+
+for i in range(x):
+    for j in range(y):
+        if i == 0 or i == x-1 or j == 0 or j == y-1:
+            print("_", end =" ")
+        else:
+            print(" ", end=" ")
+    print()
