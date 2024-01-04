4cd096ba0b8bd35238109bb9e52448338eaecb71
aryanshygun <aryanshygun5272@gmail.com>
2024-01-04 21:55:09 +0330

    commited

--- Session 3 - Homework(1-2).py
+++ Session 3 - Homework(1-2).py
@@ -1,3 +1,5 @@
+############## hollow rectangle
+
 x = int(input(" Value for the length of the Rectangle:\n"))
 y = int(input(" Value for the width of the Rectangle:\n"))
  
@@ -9,3 +11,17 @@ for i in range(x):
         else:
             print(" ", end=" ")
     print()
+
+
+
+print('\n')
+
+
+############# hollow square
+z = int(input(" Value for the sides of the square:\n"))
+
+for i in range(z):
+    if i == 0 or i == z-1:
+        print("* "*z)
+    else:
+        print("* " + "  "*(z-2) +"*")
